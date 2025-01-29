d9e1d9d94f72824cb4f9141afac9f478447d4cb5
Kazu Kuroiwa <ka_kuroiwa@waku-2.com>
2025-01-29 18:31:22 +0900

    feat: ヘッダーのSP表示の崩れを解消

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -5,6 +5,7 @@ import { getServerSession } from "@/lib/auth";
 import prisma from "../../../prisma";
 import LoginButton from "../LoginButton";
 import DropdownMenu from "../DropdownMenu";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 export default async function Header() {
   const session = await getServerSession();
@@ -21,12 +22,15 @@ export default async function Header() {
         {!!session && !!profile ? (
           <DropdownMenu image={profile?.image} name={profile?.name} bookshelfId={profile?.bookshelf_id} />
         ) : (
-          <div>
-            <Link href="/how_to_use" className={styles.howToUseLink}>
-              アプリの使いかた
-            </Link>
-            <LoginButton />
-          </div>
+          <>
+            <div className={styles.pcHeaderSideMenu}>
+              <Link href="/how_to_use" className={styles.howToUseLink}>
+                アプリの使いかた
+              </Link>
+              <LoginButton />
+            </div>
+            <HamburgerMenuIcon width={25} height={25} />
+          </>
         )}
       </div>
     </header>
