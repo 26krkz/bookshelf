8bddb07fd4c38f2c35f81af47417f0f4125224c6
26krkz <26krkz@gmail.com>
2025-02-02 12:28:21 +0900

    feat: howtouseのSP版実装

--- app/(site)/how_to_use/page.tsx
+++ app/(site)/how_to_use/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
+import getDeviceFlag from "@/_services/getDeviceFlag";
 
 export default function Page() {
+  const deviceFlag = getDeviceFlag();
+  const isPC = deviceFlag === "desktop";
   return (
     <>
       <div className={styles.descriptionArea}>
@@ -18,24 +21,24 @@ export default function Page() {
         <p>ぜひ、見せたい本だけを集めたあなただけの本棚を作り、友人や家族とシェアしてみてください。</p>
         <p className={styles.descriptionTitle}>本を検索する</p>
         <p>本棚に追加したい本を簡単に検索できます。</p>
-        <Image src="/search-area.png" alt="検索エリアの画像" width={700} height={350} className={styles.descriptionImage} />
+        <Image src="/search-area.png" alt="検索エリアの画像" width={isPC ? 700 : 300} height={isPC ? 350 : 150} className={styles.descriptionImage} />
         <p className={styles.descriptionTitle}>本棚に追加する</p>
         <p>（会員登録が必要です。）</p>
         <p>「本棚に追加する」ボタンをクリックするだけで、選んだ本を本棚に追加できます。</p>
         <p>本棚に追加した本を削除するには、もう一度「本棚に追加する」ボタンをクリックするか、マイページからアクセスできる本棚編集ページで削除が可能です。</p>
-        <Image src="/add-bookshelf.png" alt="本棚に追加するボタンの画像" width={500} height={250} className={styles.descriptionImage} />
+        <Image src="/add-bookshelf.png" alt="本棚に追加するボタンの画像" width={isPC ? 700 : 300} height={isPC ? 350 : 150} className={styles.descriptionImage} />
         <p className={styles.descriptionTitle}>本棚を閲覧する</p>
         <p>マイページから、自分のシェアする本棚を閲覧できます。</p>
         <p>
           シェアする本棚は、<span className={styles.textBold}>会員登録がなくても閲覧できるため</span>、アプリを使ったことがない人にもURLを共有して、ぜひあなたの本棚をシェアしてください。
         </p>
-        <Image src="/user-bookshelf-page.png" alt="シェアする本棚の画像" width={700} height={350} className={styles.descriptionImage} />
+        <Image src="/user-bookshelf-page.png" alt="シェアする本棚の画像" width={isPC ? 700 : 300} height={isPC ? 350 : 150} className={styles.descriptionImage} />
         <p className={styles.descriptionTitle}>お気に入りリストに追加する</p>
         <p>（会員登録が必要です。）</p>
         <p>本棚に追加する予定はないけれど、後で読みたい本や気になる本を「お気に入りリスト」に追加して管理できます。</p>
         <p>お気に入りリストから本棚に追加することも可能です。</p>
         <p>お気に入りリストから削除するには、もう一度「お気に入りに追加する」ボタンを押すか、マイページの編集ページで削除できます。</p>
-        <Image src="/add-favorite.png" alt="お気に入りに追加するボタンの画像" width={500} height={250} className={styles.descriptionImage} />
+        <Image src="/add-favorite.png" alt="お気に入りに追加するボタンの画像" width={isPC ? 700 : 300} height={isPC ? 350 : 150} className={styles.descriptionImage} />
       </div>
     </>
   );
